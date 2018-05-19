76fed01775f800b72beac5a9e0063ace7671d893
lumitrail <lumitrail@gmail.com>
2018-05-19 20:02:13 +0900

    Swipe!

--- app/src/main/java/com/example/dongju/keyman/keymankeyboard.java
+++ app/src/main/java/com/example/dongju/keyman/keymankeyboard.java
@@ -11,7 +11,6 @@ import android.view.inputmethod.InputConnection;
 
 public class keymankeyboard extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
 
-    private int y1, y2;
     private int InitialPrimaryCode;
 
     private KeyboardView kv;
@@ -33,24 +32,9 @@ public class keymankeyboard extends InputMethodService implements KeyboardView.O
         return kv;
     }
 
-    public boolean onTouchEvent(MotionEvent event) {
-        switch(event.getAction() & MotionEvent.ACTION_MASK) {
-            case MotionEvent.ACTION_DOWN:
-                //x1 = (int) (event.getX());
-                y1 = (int) (event.getY());
-                break;
-
-            case MotionEvent.ACTION_UP:
-                //x2 = (int) (event.getX());
-                y2 = (int) (event.getY());
-                break;
-        }
-        return true;
-    }
-
     @Override
     public void onKey (int primaryCode, int[] ints) {
-
+        playClick(primaryCode);
     }
 
     private void playClick(int keyCode) {
@@ -78,38 +62,90 @@ public class keymankeyboard extends InputMethodService implements KeyboardView.O
 
     @Override
     public void onPress (int primaryCode) {
-        playClick(primaryCode);
         InitialPrimaryCode = primaryCode;
     }
 
     @Override
     public void onRelease (int primaryCode) {
         InputConnection ic = getCurrentInputConnection();
+        char code;
         switch (InitialPrimaryCode)
         {
             case Keyboard.KEYCODE_DELETE:
                 ic.deleteSurroundingText(1,0);
-                break;
+                return;
             case Keyboard.KEYCODE_SHIFT:
                 isCaps = !isCaps;
                 keyboard.setShifted(isCaps);
                 kv.invalidateAllKeys();
-                break;
+                return;
             case Keyboard.KEYCODE_DONE:
                 ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                return;
+
+            case 97: // a, b, c
+                if(primaryCode == 101){ // swipe up to e
+                    code = (char)(InitialPrimaryCode +1);
+                }
+                else if(InitialPrimaryCode < primaryCode) { // swipe up further
+                    code = (char) (InitialPrimaryCode +2);
+                }
+                else {
+                    code = (char)InitialPrimaryCode;
+                }
+                break;
+            case 105: //i, j
+                if(primaryCode < InitialPrimaryCode){ // swipe down...
+                    code = (char)(InitialPrimaryCode +1);
+                }
+                else{
+                    code = (char)InitialPrimaryCode;
+                }
+                break;
+            case 104: // h, only downward swipe is.
+            case 108: // l
+            case 110: // n
+            case 114: // r
+                if(primaryCode < InitialPrimaryCode){ //swipe down!
+                    code = (char)(InitialPrimaryCode -1);
+                }
+                else{
+                    code = (char)InitialPrimaryCode;
+                }
+                break;
+            case 111: // o, only upward swipe is.
+                if(InitialPrimaryCode < primaryCode) { // swipe up
+                    code = (char)(InitialPrimaryCode +1);
+                }
+                else{
+                    code = (char)InitialPrimaryCode;
+                }
+                break;
+            case 121: // y.. it's with only upward swipe, but no keys above. so this is a temporary solution, making r key wider.
+                if(primaryCode == 114){ // swipe up to r
+                    code = (char)(InitialPrimaryCode +1);
+                }
+                else {
+                    code = (char)InitialPrimaryCode;
+                }
                 break;
             default:
-                char code = (char)InitialPrimaryCode;
-                if(Character.isLetter(code) && isCaps){
-                    code = Character.toUpperCase(code);
+                if(InitialPrimaryCode < primaryCode){ // swipe up!
+                    code = (char)(InitialPrimaryCode +1);
                 }
-                //if(y2 > y1+20){
-                    ic.commitText(String.valueOf(code+1),1);
-                //}
-                //else{
-                    //ic.commitText(String.valueOf(code),1);
-                //}
+                else if(primaryCode < InitialPrimaryCode){ // swipe down!
+                    code = (char)(InitialPrimaryCode -1);
+                }
+                else{
+                    code = (char)InitialPrimaryCode;
+                }
         }
+
+        if(Character.isLetter(code) && isCaps){
+            code = Character.toUpperCase(code);
+        }
+        ic.commitText(String.valueOf(code),1);
+
     }
 
     @Override
